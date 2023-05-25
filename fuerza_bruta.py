c7164679fc12bc47aff4ad9091fb0a54dce87e23
Emerson Espinoza <xinayespinoza@gmail.com>
2023-05-24 21:26:27 -0400

    fuerza bruta dinamico

---
+++ fuerza_bruta.py
@@ -0,0 +1,26 @@
+import itertools
+import string
+import getpass
+
+# password_objetivo = "@bDa"  # password que queremos encontrar
+def probar_password(password):
+    return password == password_objetivo
+
+def buscar_password(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    combinaciones = itertools.product(caracteres, repeat=longitud)
+    contador = 0
+    for combinacion in combinaciones:
+        contador += 1
+        password = ''.join(combinacion)
+        print(f"combinación {contador} : {password}  ")
+        if probar_password(password):
+            print("password encontrada:", password)
+            print(f"Numero de combinaciones:{contador}")
+            return True
+    print("No se encontró ninguna coincidencia.")
+    print(f"Numero de combinaciones:{contador}")
+    return False
+contra = int(input("ingrese un número para el tamaño de longitud de su password: "))
+password_objetivo = getpass.getpass(f"Ingrese su password de {contra} caracteres: ")
+buscar_password(contra)
